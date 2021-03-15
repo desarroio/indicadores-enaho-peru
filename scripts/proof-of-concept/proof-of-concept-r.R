71d927a3b981013730a3e392546db9e1b3258087
gtalavera <gonzalo.talavera@gmail.com>
2021-03-14 21:34:28 -0500

    comentarios

--- scripts/proof-of-concept/proof-of-concept-r.R
+++ scripts/proof-of-concept/proof-of-concept-r.R
@@ -3,40 +3,30 @@ library(haven)
 library(survey)
 library(readr)
 
-enaho01_2019_100 <- read_dta("data/raw/enaho01-2019-100.dta")
-stata_results <- read_csv("outputs/proof-of-concept/stata-results.csv")
+enaho01_2019_100 <- read_dta("data/raw/enaho01-2019-100.dta") #cargar data
+stata_results <- read_csv("outputs/proof-of-concept/stata-results.csv") #cargar resultados en stata
 
 enaho01_2019_100 <- enaho01_2019_100 %>%
-  filter( result == 1 | result == 2) %>% #observaciones completas o incompletas
+  filter( result == 1 | result == 2) %>% #filtra observaciones completas o incompletas
   select( aÑo:panel, p103, p110, p111a, p1121, p1136, p1137, factor07) %>%   #selccionar variables
   mutate( dpto = as_factor(str_sub( ubigeo, 1, 2)),   #Creando variable de dpto, distrito y total
           dist = as_factor(ubigeo),
           total = "Total",
           dominio_enaho = as_factor( dominio),
-          piso_tierra      = case_when( is.na( p103) ~ NA_integer_,
-                                        p103 %in% c( 6, 7) ~ 1L,
-                                        TRUE ~ 0L), #creando indicadores
-          agua_red_publica = case_when( is.na( p110) ~ NA_integer_,
-                                        p110 %in% c( 1, 2) ~ 1L,
-                                        TRUE ~ 0L),
-          desague_red_publica = case_when( is.na( p111a) ~ NA_integer_,
-                                        p111a %in% c( 1, 2) ~ 1L,
-                                        TRUE ~ 0L),
+          #creando indicadores
+          piso_tierra      = case_when( is.na( p103) ~ NA_integer_, p103 %in% c( 6, 7) ~ 1L, TRUE ~ 0L), 
+          agua_red_publica = case_when( is.na( p110) ~ NA_integer_, p110 %in% c( 1, 2) ~ 1L, TRUE ~ 0L),
+          desague_red_publica = case_when( is.na( p111a) ~ NA_integer_, p111a %in% c( 1, 2) ~ 1L, TRUE ~ 0L),
           electricidad = as.integer(p1121),
-          cocina_lena =  case_when( is.na( p1136) | is.na( p1137) ~ NA_integer_,
-                                    p1136 == 1 | p1137 == 1 ~ 1L,
-                                   TRUE ~ 0L),
+          cocina_lena =  case_when( is.na( p1136) | is.na( p1137) ~ NA_integer_, p1136 == 1 | p1137 == 1 ~ 1L, TRUE ~ 0L),
           cod_viv = paste0( conglome, vivienda, hogar)
           ) 
 
 # diseño de encuesta - se asume que los conglomerados ya incluyen la primera etapa de la selección y por 
 # lo tanto se considera com UPM (a pesr de que en realidad es la segunda sefun ficha técnica)
-enaho_design <- svydesign( data = enaho01_2019_100,
-                           weights = ~factor07,
-                           id = ~conglome+cod_viv,
-                           strata = ~dpto)
+enaho_design <- svydesign( data = enaho01_2019_100, weights = ~factor07, id = ~conglome+cod_viv, strata = ~dpto)
 
-#función para calcular survey indicators
+#función muy poco elegante para calcular survey indicators para los 4 niveles
 calc <- function( grupo) {
   a <- svyby( formula = ~piso_tierra + agua_red_publica + desague_red_publica + electricidad + cocina_lena,
          by = grupo,
@@ -55,19 +45,19 @@ calc <- function( grupo) {
   d <- pivot_wider( c, names_from = tipo, values_from = resultado)
 }
 
-#calculo de indicadores
-lista <- list( ~total, ~dominio_enaho, ~dpto, ~dist)
+#calculo de indicadores aplicando función
+lista <- list( ~total, ~dominio_enaho, ~dpto, ~dist) #lista de niveles
 lista2 <- lapply( lista, calc)
 lista3 <- lapply( lista2, rename, grupo = 1 )
 
 r_results <- bind_rows(lista3)
 
-write_csv(r_results, "outputs/proof-of-concept/r_results.csv")
+write_csv(r_results, "outputs/proof-of-concept/r_results.csv") #guardar .csv
 
+#crear tabla de comparación
 comparacion <- 
   left_join( stata_results, r_results, by = c("grupo", "indicador"), suffix = c( ".stata", ".r")) %>%
-  mutate( dif_res = resultado.stata-resultado.r,
-                        dif_cv= cv.stata-cv.r)
+  mutate( dif_res = resultado.stata-resultado.r, dif_cv= cv.stata-cv.r)
 
 #Reporte de comparación
 summary( comparacion %>% select(dif_res, dif_cv))
